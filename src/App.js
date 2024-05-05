b2fb8fe34021c7ba669e312f01796e76e76e3835
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-05-05 22:42:57 +0530

    Making Banner of Visiting Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar.jsx';
-
+import { Banner } from './components/Banner.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
