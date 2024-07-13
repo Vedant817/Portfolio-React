1d3da6ba0442a7850ba4ea8e7ef4ebc475361038
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-13 11:23:08 +0530

    Project Section Completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
