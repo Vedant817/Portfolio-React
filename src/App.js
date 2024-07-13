dbac6f1aa8c46c2c97ecc1112c9162c18128b56c
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-13 12:04:14 +0530

    Contact Page Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { NavBar } from './components/NavBar.jsx';
 import { Banner } from './components/Banner.jsx';
 import { Skills } from './components/Skills.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Projects from './components/Projects.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
